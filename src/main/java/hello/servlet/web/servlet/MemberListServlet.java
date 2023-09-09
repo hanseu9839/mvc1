d3097fa3d831b0fe921b3ae6758bd0d5e3120c13
SeogunGyun <hanseu9839@gmail.com>
2023-09-09 23:28:31 +0900

    JspPattern

--- src/main/java/hello/servlet/web/servlet/MemberListServlet.java
+++ src/main/java/hello/servlet/web/servlet/MemberListServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 @WebServlet(name="memberListServlet", urlPatterns = "/servlet/members")
@@ -21,5 +22,32 @@ public class MemberListServlet extends HttpServlet {
 
         response.setContentType("text/html");
         response.setCharacterEncoding("utf-8");
+        PrintWriter w = response.getWriter();
+        w.write("<html>");
+        w.write("<head>");
+        w.write("    <meta charset=\"UTF-8\">");
+        w.write("    <title>Title</title>");
+        w.write("</head>");
+        w.write("<body>");
+        w.write("<a href=\"/index.html\">메인</a>");
+        w.write("<table>");
+        w.write("    <thead>");
+        w.write("    <th>id</th>");
+        w.write("    <th>username</th>");
+        w.write("    <th>age</th>");
+        w.write("    </thead>");
+        w.write("    <tbody>");
+
+        for (Member member : members) {
+            w.write("    <tr>");
+            w.write("        <td>" + member.getId() + "</td>");
+            w.write("        <td>" + member.getUsername() + "</td>");
+            w.write("        <td>" + member.getAge() + "</td>");
+            w.write("    </tr>");
+        }
+        w.write("    </tbody>");
+        w.write("</table>");
+        w.write("</body>");
+        w.write("</html>");
     }
 }
